1276d53ef9d1c44dbd82902b86be5c9c42fefc04
unknown <pawelkowalski99@gmail.com>
2022-10-21 19:25:01 +0200

    feat: updated GetEstates crawler

--- core/infrastructure/crawler/crawler.go
+++ core/infrastructure/crawler/crawler.go
@@ -28,12 +28,6 @@ type OtoDom struct {
 	b *rod.Browser
 
 	domain string
-
-	mode string
-
-	estateType string
-
-	city string
 }
 
 func NewOtodom(br *rod.Browser) *OtoDom {
@@ -43,35 +37,34 @@ func NewOtodom(br *rod.Browser) *OtoDom {
 	}
 }
 
-func (s *OtoDom) GetEstates(mode, city, estateType string, pageNum int) ([]entities.Estate, error){
-	p := s.b.MustPage(fmt.Sprintf(queryUrl, s.mode, s.estateType, s.city, strconv.Itoa(pageNum))).MustWaitLoad()
+func (s *OtoDom) GetEstates(mode, city, estateType string, pageNum int) (estates []entities.Estate, err error){
+	p := s.b.MustPage(fmt.Sprintf(queryUrl, mode, estateType, city, strconv.Itoa(pageNum))).MustWaitLoad()
 	fmt.Println(pageNum, p.MustInfo())
 
-	estatesElements := p.MustElementsX("//*[@id=\"__next\"]/div[2]/main/div/div[2]/div[1]/div[2]/div[1]/div/ul/li")
-	//fmt.Println(estatesElements)
+	estatesElements := p.MustElementsX("//*[@id=\"__next\"]/div[2]/main/div/div[2]/div[1]/div[2]/div[1]/div/ul/li/a")
 
-	estatesElements = append(estatesElements, p.
-		MustElementsX("//*[@id=\"__next\"]/div[2]/main/div/div[2]/div[1]/div[2]/div/ul/li")...)
+	estatesElements = append(estatesElements,
+		p.MustElementsX("//*[@id=\"__next\"]/div[2]/main/div/div[2]/div[1]/div[2]/div/ul/li/a")...)
 
 	for _, estateEl := range estatesElements {
-		attrs := estateEl.MustElementsX("a/article/div[1]/span")
-		fmt.Println(estateEl.MustElementX("/a").MustHTML())
+		attrs := estateEl.MustElementsX("article/div[2]/span")
 		estates = append(estates, entities.Estate{
-			URL: "",
-			Address:              estateEl.MustElementX("article/p/span").MustText(),
-			Price:                attrs[0].MustText(),
-			PricePerM2:           attrs[1].MustText(),
-			RoomAmount:           attrs[2].MustText(),
-			Surface:              attrs[3].MustText(),
+			URL:         estateEl.MustProperty("href").String(),
+			Address:     estateEl.MustElementX("article/p/span").MustText(),
+			Price:       attrs[0].MustText(),
+			PricePerM2:  attrs[1].MustText(),
+			RoomAmount:  attrs[2].MustText(),
+			Surface:     attrs[3].MustText(),
 		})
 	}
+
 
 	// Recursive navigation to the last page
 	if p.
 		MustElementsX("//*[@id=\"__next\"]/div[2]/main/div/div[2]/div[1]/div[4]/div/nav/button").Last().
 		MustAttribute("disabled") == nil {
 			pageNum++
-			estatesHrefTemp, err2 := s.GetEstates(pageNum)
+			estatesHrefTemp, err2 := s.GetEstates(mode, city, estateType,pageNum)
 			if err2 != nil {
 				log.Infof(err2.Error())
 				return nil, err2
